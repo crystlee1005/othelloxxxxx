0f9dc0c6c0e010e08e80cf88607e719f84edf31a
crystlee1005 <crystlee1005@sookmyung.ac.kr>
2020-11-10 01:20:42 +0900

    gameboard

--- gameboard.c
+++ gameboard.c
@@ -3,18 +3,37 @@
 #define N 6
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main() {    
+char gameboard[N][N];
+
+int main(void){
+  // 필요한 변수들 정의 
+  int i, j;
+  char b, w;
+  b = '@'; // 검정색 알
+  w = 'O'; // 하얀색 알 
+  
+  for(i=0; i<N; i++)
+     for(j=0; j<N; j++) {
+     	gameboard[i][j] = b;    	
+	 }
+
+
+   
 
 // for문을 활용- N줄의 보드판을 출력하는 함수   
 
-	int i;
-	for(i=0; i<N; i++) {
-    	printf("-------------------\n|  |  |  |  |  |  |\n");
+printf("-------------------------\n");
+	for(i=0; i<N; i++)
+	{
+	 for(j=0; j<N; j++)
+	 
+    	printf("| %c ", gameboard[i][j]);
     	
-	}	
-	printf("-------------------\n");
-
-	
+	 	
+	printf("|\n");	
+	printf("-------------------------\n");
+    
+    }
 
 	return 0;
 }
