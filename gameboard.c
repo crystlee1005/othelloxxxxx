e508b2cb630529b614d67f6dbcf347c9a3c3449b
crystlee1005 <crystlee1005@sookmyung.ac.kr>
2020-11-09 23:48:34 +0900

    gameboard

---
+++ gameboard.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 6
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {    
+
+// for문을 활용- N줄의 보드판을 출력하는 함수   
+
+	int i;
+	for(i=0; i<N; i++) {
+    	printf("-------------------\n|  |  |  |  |  |  |\n");
+    	
+	}	
+	printf("-------------------\n");
+
+	
+
+	return 0;
+}
