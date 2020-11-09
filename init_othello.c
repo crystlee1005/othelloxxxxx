b997f33081c2eb88c443667b20dfb9faa4aed6f5
crystlee1005 <crystlee1005@sookmyung.ac.kr>
2020-11-10 01:02:14 +0900

    init_othello

--- init_othello.c
+++ init_othello.c
@@ -3,7 +3,7 @@
 #define N 6
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int gameboard[N][N];
+char gameboard[N][N];
 
 int main(void){
   // 필요한 변수들 정의 
@@ -16,13 +16,27 @@ int main(void){
      for(j=0; j<N; j++) {
      	gameboard[i][j];     	
 	 }
-     
+
     // 게임 초기화_ 정중앙 4칸에 흰색 검정색 알을 2개씩 대각선 모양으로 배치
+void init_othello(char gameboard[][N])
+{
 
    gameboard[N/2][N/2] = b;
    gameboard[N/2][N/2 + 1] = w;
    gameboard[N/2 + 1][N/2] = w;
    gameboard[N/2 + 1][N/2 + 1] = b;
-   
+
+   // 초기값을 인쇄  
+    
+     for(i=0; i<N; i++)
+     {
+	   for(j=0; j<N; j++)
+	    {
+     	printf("%c", gameboard[i][j]);
+     	printf("\n");
+        }
+
+	 }
+} 
 }
 
